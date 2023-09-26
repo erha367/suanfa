5fd4e0532060a2496bd975214012954abed1c2f7
yang.sen <v_yangsen08@baidu.com>
2023-09-26 13:13:38 +0800

    合并2个有序数组

--- 70.jump_test.go
+++ 70.jump_test.go
@@ -4,10 +4,12 @@ import "testing"
 
 // 青蛙跳台阶
 func TestJump(t *testing.T) {
-	n := 50
+	n := 5
 	tmp := make(map[int]int)
 	res := Jump(n, tmp)
 	t.Log(res)
+	res2 := Jump2(n)
+	t.Log(res2)
 }
 
 // 递归+优化
@@ -20,5 +22,25 @@ func Jump(n int, tmp map[int]int) int {
 	}
 	res := Jump(n-1, tmp) + Jump(n-2, tmp)
 	tmp[n] = res
+	return res
+}
+
+// 递归改循环
+func Jump2(n int) int {
+	if n < 3 {
+		return n
+	}
+	//保存结果
+	res := 0
+	//中间过程f(2)
+	pre := 2
+	//中间过程f(1)
+	prepre := 1
+	//3~n的数字区间
+	for i := 3; i < n+1; i++ {
+		res = pre + prepre
+		prepre = pre
+		pre = res
+	}
 	return res
 }
---
+++ 88.mergearr_test.go
@@ -0,0 +1,37 @@
+package suanfa
+
+import (
+	"testing"
+)
+
+//给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+
+func TestMergeArray(t *testing.T) {
+	a := []int{1, 3, 5, 7, 10, 20}
+	b := []int{2, 4, 60}
+	res := MergeArray(a, b)
+	t.Log(res)
+}
+
+func MergeArray(a []int, b []int) []int {
+	var res []int
+	var i, j int
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			res = append(res, a[i])
+			i++
+		} else {
+			res = append(res, b[j])
+			j++
+		}
+	}
+	for i < len(a) {
+		res = append(res, a[i])
+		i++
+	}
+	for j < len(b) {
+		res = append(res, b[j])
+		j++
+	}
+	return res
+}
