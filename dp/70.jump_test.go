4ca4fe1a37fc23d3b5ca7b71fef70c251a5e2279
yang.sen <v_yangsen08@baidu.com>
2023-10-25 10:32:39 +0800

    优化了青蛙跳

--- dp/70.jump_test.go
+++ dp/70.jump_test.go
@@ -1,46 +1,33 @@
 package dp
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
+
+/*
+一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+求斐波那契数列第N项
+*/
 
 // 青蛙跳台阶
 func TestJump(t *testing.T) {
 	n := 5
-	tmp := make(map[int]int)
-	res := Jump(n, tmp)
+	res := Jump(n)
 	t.Log(res)
-	res2 := Jump2(n)
-	t.Log(res2)
 }
 
-// 递归+优化
-func Jump(n int, tmp map[int]int) int {
-	if n < 3 {
-		return n
+// 动态规划
+func Jump(n int) int {
+	dp := make([]int, n+1)
+	dp[0] = 1
+	dp[1] = 1
+	if n <= 1 {
+		return 1
 	}
-	if val, ok := tmp[n]; ok {
-		return val
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
 	}
-	res := Jump(n-1, tmp) + Jump(n-2, tmp)
-	tmp[n] = res
-	return res
-}
-
-// 递归改循环
-func Jump2(n int) int {
-	if n < 3 {
-		return n
-	}
-	//保存结果
-	res := 0
-	//中间过程f(2)
-	pre := 2
-	//中间过程f(1)
-	prepre := 1
-	//3~n的数字区间
-	for i := 3; i < n+1; i++ {
-		res = pre + prepre
-		prepre = pre
-		pre = res
-	}
-	return res
+	log.Println(dp)
+	return dp[n]
 }
